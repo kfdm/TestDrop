2aef0c030733c0151c64e3b3848d131518d132f9
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-12-01 20:12:55 +0900

    Initial Commit

---
+++ TestDrop/TestDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDropApp.swift
+//  TestDrop
+//
+//  Created by Paul Traylor on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct TestDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
