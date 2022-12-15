5b80b75a7e88a144faa5d292eb35d7ebe32f66e0
Paul Traylor <kungfudiscomonkey@gmail.com>
2022-12-15 19:20:37 +0900

    Test

--- TestDrop/ContentView.swift
+++ TestDrop/ContentView.swift
@@ -1,26 +1,48 @@
-//
-//  ContentView.swift
-//  TestDrop
-//
-//  Created by Paul Traylor on 2022/12/01.
-//
-
 import SwiftUI
+import UniformTypeIdentifiers
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+let testType = UTType.text
+
+struct DropTest: DropDelegate {
+    func performDrop(info: DropInfo) -> Bool {
+        print(info)
+        for item in info.itemProviders(for: [testType]) {
+            print("Item: \(item.debugDescription) \(type(of: item))")
+            item.loadItem(forTypeIdentifier: testType.identifier) { obj, err in
+                print("loadItem")
+                print(obj)
+                print(err)
+            }
+            _ = item.loadObject(ofClass: String.self) { obj, error in
+                print("\(type(of: obj))")
+                guard error == nil else { return print("Failed to load our dragged item. \(error.debugDescription)") }
+                print("drop item: \(obj.debugDescription)")
+            }
         }
-        .padding()
+        print("Something received")
+        return true
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @State var source : [String] = ["A", "B", "C"]
+    @State var dest : [String] = ["D","E","F"]
+    var body: some View {
+        HStack {
+            List {
+                ForEach(source, id: \.self) { element in
+                    Text(element)
+                        .onDrag { NSItemProvider(object: element as NSString) }
+                }
+            }
+            List {
+                ForEach(dest, id: \.self) { element in
+                    Text(element)
+                }
+                .onDrop(of: [testType.identifier], delegate: DropTest())
+            }
+
+        }
+        .padding()
     }
 }
